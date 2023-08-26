3b5fa41e48ed6f27a4b0cb8651b44d885a6a623f
aarishgilani <aarishgilani10@gmail.com>
2023-08-26 13:16:16 -0500

    Set default attribute array for view function

--- core/functions.php
+++ core/functions.php
@@ -5,6 +5,7 @@ function dd($var) {
     var_dump($var);
 
     die();
+
 }
 
 function base_path($path) {
@@ -12,9 +13,26 @@ function base_path($path) {
     global $base_path;
 
     return $base_path . $path;
+
 }
 
-function view($name, $attributes) {
+function view($name, $attributes = []) {
+
     extract($attributes);
+
     return require base_path("views/{$name}.view.php");
+
+}
+
+function url($path) {
+
+    global $url;
+
+    if($path === $url) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 }
