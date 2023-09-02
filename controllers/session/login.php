38a4100e6c0b4b629ff8deb121ad425478cd3976
aarishgilani <aarishgilani10@gmail.com>
2023-09-01 19:58:45 -0500

    Redirect home if user is logged in and hits /login

--- controllers/session/login.php
+++ controllers/session/login.php
@@ -1,5 +1,12 @@
 <?php
 
+// Return home if logged in
+if(Session::get('logged_in') ?? false) {
+    header('location: /');
+
+    exit();
+}
+
 view('session/form', [
     'errors' => $_SESSION['_flash']['errors'] ?? NULL
 ]);
