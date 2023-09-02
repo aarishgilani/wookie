e07cb75cf479cb1dcc61d09c59aa164c1edb0daf
aarishgilani <aarishgilani10@gmail.com>
2023-09-02 10:07:43 -0500

    Authenticat user using PRG pattern else flash session error.

--- controllers/session/auth.php
+++ controllers/session/auth.php
@@ -11,20 +11,26 @@ $user = $db->query('select * from users where email = :email and password = :pas
 ])->findOrFail();
 
 if($user) {
+    
+    // Login if password matches
+    if($user['password'] === $_POST['password']) {
+        Session::put('logged_in', true);
+        Session::put('email', $user['email']);
+        Session::put('name', $user['name']);
 
-    Session::put('logged_in', true);
-    Session::put('email', $user['email']);
-    Session::put('name', $user['name']);
+        header('location: /notes');
 
-    header('location: /notes');
-
-    exit();
+        exit();
+    }
 
 }
 
-return view('session/view',[
-    'errors' => []
-]);
+// Redirect using PRG pattern and pass errors
+Session::flash('errors', 'Could not find the an account matching given credentials.');
+
+header('location: /login');
+
+exit();
 
 
 
