2ddbf4a43abd6e48847c6dbe3af072233bec3cdc
aarishgilani <aarishgilani10@gmail.com>
2023-09-02 10:09:28 -0500

    Add name field to registration form

--- views/registrations/create.view.php
+++ views/registrations/create.view.php
@@ -1,25 +1,25 @@
-<?php if($errors): ?>
-<div class="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-  <p class="text-sm leading-6 text-white">
-    <a href="#">
-      <?= $errors ?>
-    </a>
-  </p>
-  <div class="flex flex-1 justify-end">
-    <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
-      <span class="sr-only">Dismiss</span>
-      <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-      </svg>
-    </button>
+<?php if ($errors) : ?>
+  <div class="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <p class="text-sm leading-6 text-white">
+      <a href="#">
+        <?= $errors ?>
+      </a>
+    </p>
+    <div class="flex flex-1 justify-end">
+      <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+        <span class="sr-only">Dismiss</span>
+        <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+        </svg>
+      </button>
+    </div>
   </div>
-</div>
 <?php endif; ?>
 
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <span class="text-center flex justify-center">
-        <?php icon('wookie') ?>
+      <?php icon('wookie') ?>
     </span>
     <h1 class="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-indigo-900">Wookie</h1>
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create a new account</h2>
@@ -27,6 +27,13 @@
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" action="/register" method="POST">
+      <div>
+        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+        <div class="mt-2">
+          <input id="name" name="email" type="name" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <div class="mt-2">
@@ -62,7 +69,7 @@
       <a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login here</a>
     </p>
     <p class="mt-10 text-center text-sm text-gray-500">
-        Crafted with passion by <a class="font-semibold text-indigo-600 hover:text-indigo-500" target="_blank" href="https://aarishgilani.com"> Aarish </a>
+      Crafted with passion by <a class="font-semibold text-indigo-600 hover:text-indigo-500" target="_blank" href="https://aarishgilani.com"> Aarish </a>
     </p>
   </div>
 </div>
