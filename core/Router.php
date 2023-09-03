8585264db2758a6c7c8ed48a4f84e4913ff519dc
aarishgilani <aarishgilani10@gmail.com>
2023-09-03 16:22:04 -0500

    Use namspace and add a method to flush session

--- core/Router.php
+++ core/Router.php
@@ -1,75 +1,90 @@
 <?php
 
-class Router {
+namespace Core;
 
-    protected $routes = [];
+use Core\Middleware\Middleware;
 
-    public function add($method, $controller, $url)
+class Router
+{
+
+    public $routes = [];
+
+    protected $url;
+
+    protected $method;
+
+    public function add($method, $controller, $url, $middleware = null)
     {
 
         $this->routes[] = [
             'url' => $url,
             'controller' => $controller,
-            'method' => $method
+            'method' => $method,
+            'middleware' => $middleware
         ];
 
+        return $this;
     }
 
     public function get($url, $controller)
     {
 
-        $this->add('GET', $controller, $url);
-
+        return $this->add('GET', $controller, $url);
     }
 
     public function post($url, $controller)
     {
 
-        $this->add('POST', $controller, $url);
-
+        return $this->add('POST', $controller, $url);
     }
 
     public function delete($url, $controller)
     {
 
-        $this->add('DELETE', $controller, $url);
-        
+        return $this->add('DELETE', $controller, $url);
     }
 
     public function patch($url, $controller)
     {
 
-        $this->add('PATCH', $controller, $url);
-        
+        return $this->add('PATCH', $controller, $url);
     }
 
     public function put($url, $controller)
     {
 
-        $this->add('PUT', $controller, $url);
-        
+        return $this->add('PUT', $controller, $url);
     }
 
     public function route($url, $method)
     {
 
-        foreach($this->routes as $route)
-        {
-            if( $url === $route['url'] && $method === $route['method'] ) {
-                return require base_path($route['controller']) ;
+        foreach ($this->routes as $route) {
+
+            if ($url === $route['url'] && $method === $route['method']) {
+
+                if ($route['middleware']) {
+                    Middleware::resolve($route['middleware']);
+                }
+
+                return require base_path($route['controller']);
             }
         }
 
+        $this->abort();
+    }
+
+    public function only($middleware)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $middleware;
     }
 
     protected function abort($code = 404)
     {
-
-        // add view redirection here
+        view('abort');
 
         http_response_code($code);
 
         exit();
     }
-
 }
