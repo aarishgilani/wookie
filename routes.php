e9b49dcae374e3b8aa6f85ee7a26c6bcba65f859
aarishgilani <aarishgilani10@gmail.com>
2023-09-03 16:19:21 -0500

    Cleanup and better comments

--- routes.php
+++ routes.php
@@ -1,32 +1,37 @@
 <?php
 
-// return [
-//     '/' => 'controllers/home.php',
-//     '/about' => 'controllers/about.php',
-//     '/notes' => 'controllers/notes.php',
-//     '/note' => 'controllers/note.php',
-//     '/note-create' => 'controllers/note-create.php',
-//     '/contact' => 'views/contact.view.php'
-// ];
-
-
-
+/**
+ * Application Routes
+ * 
+ * Available Methods:
+ *  GET => Page routing
+ *  POST => Create new data
+ *  PUT/PATCH => Update exisiting data
+ *  DELETE => Delete exisiting data
+ * 
+ * Middleware Methods (only):
+ *  Null => Allow all
+ *  Guest => Only allow users who are not yet signed in
+ *  Auth => Only allow signed in users
+ * 
+ */
 
 $router->get('/', 'controllers/home.php');
 $router->get('/about', 'controllers/about.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
-$router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes/create', 'controllers/notes/store.php');
-$router->delete('/notes', 'controllers/notes/destroy.php');
-$router->put('/notes', 'controllers/notes/update.php');
-$router->get('/note/edit', 'controllers/notes/edit.php');
-$router->get('/note', 'controllers/notes/show.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
+$router->get('/notes/create', 'controllers/notes/create.php')->only('auth');
+$router->post('/notes/create', 'controllers/notes/store.php')->only('auth');
+$router->delete('/notes', 'controllers/notes/destroy.php')->only('auth');
+$router->put('/notes', 'controllers/notes/update.php')->only('auth');
+$router->get('/note/edit', 'controllers/notes/edit.php')->only('auth');
+$router->get('/note', 'controllers/notes/show.php')->only('auth');
 
-$router->get('/register', 'controllers/registrations/create.php');
-$router->post('/register', 'controllers/registrations/store.php');
+$router->get('/register', 'controllers/registrations/create.php')->only('guest');
+$router->post('/register', 'controllers/registrations/store.php')->only('guest');
 
-$router->get('/login', 'controllers/session/login.php');
-$router->post('/login', 'controllers/session/auth.php');
+$router->get('/login', 'controllers/session/login.php')->only('guest');
+$router->post('/login', 'controllers/session/auth.php')->only('guest');
+$router->get('/logout', 'controllers/session/logout.php')->only('auth');
 
 $router->get('/contact', 'controllers/contact.php');
