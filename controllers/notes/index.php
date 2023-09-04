619bcdc57b02873aa895ed1d81772b1be6ee1159
aarishgilani <aarishgilani10@gmail.com>
2023-09-04 12:10:44 -0500

    Fetch notes only for the loggedin user

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -6,7 +6,9 @@ $heading = "Notes";
 
 $db = new Database;
 
-$notes = $db->query('select * from notes');
+$notes = $db->query('select * from notes where user_id = :user_id', [
+    ':user_id' => $_SESSION['user_id']
+]);
 $notes = $notes->findAllOrFail();
 
 
