6b90ec76acce92e85c0542df2749b3924d4d50f2
aarishgilani <aarishgilani10@gmail.com>
2023-09-07 12:26:13 -0500

    Refactor login authentication as per authenticator class.

--- controllers/session/auth.php
+++ controllers/session/auth.php
@@ -3,6 +3,7 @@
 // receiving some data in post global variable.
 // validate it against users table to find or fail the user account
 
+use Core\Authenticator;
 use Core\Database;
 use Core\Session;
 
@@ -12,28 +13,13 @@ $user = $db->query('select * from users where email = :email', [
     ':email' => $_POST['email'],
 ])->findOrFail();
 
-if($user) {
-    
-    // Login if password matches
-    if(password_verify($_POST['password'], $user['password'])) {
-        Session::put('logged_in', true);
-        Session::put('email', $user['email']);
-        Session::put('user_id', $user['id']);
-        Session::put('name', $user['name']);
-
-        header('location: /notes');
-
-        exit();
-    }
-
+if(Authenticator::attempt($user, $_POST['password'])) {
+    redirect('/notes');
 }
 
 // Redirect using PRG pattern and pass errors
 Session::flash('errors', 'Could not find the an account matching given credentials.');
-
-header('location: /login');
-
-exit();
+redirect('/login');
 
 
 
