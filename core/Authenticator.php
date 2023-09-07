4e5c94bcac02ad009f6ad9ff8859c7f2f9acf1fb
aarishgilani <aarishgilani10@gmail.com>
2023-09-07 13:05:20 -0500

    Include the logic to find user in db inside Authenticator class.

--- core/Authenticator.php
+++ core/Authenticator.php
@@ -4,10 +4,21 @@ namespace Core;
 
 class Authenticator
 {
-    public static function attempt($user, $password)
-    {
 
-        if ( password_verify($password, $user['password']) ) {
+    public function find($email)
+    {
+        $db = new Database;
+
+        return $db->query('select * from users where email = :email', [
+            ':email' => $email,
+        ])->findOrFail();
+    }
+
+    public function attempt($email, $password)
+    {
+        $user = $this->find($email);
+
+        if (password_verify($password, $user['password'])) {
 
             Session::put('logged_in', true);
             Session::put('user_id', $user['id']);
@@ -18,6 +29,5 @@ class Authenticator
         }
 
         return false;
-
     }
 }
