d9d633b06223e11a7db907b33e187d3f38af6a77
aarishgilani <aarishgilani10@gmail.com>
2023-09-07 13:05:45 -0500

    Further clean up aut router

--- controllers/session/auth.php
+++ controllers/session/auth.php
@@ -1,25 +1,12 @@
 <?php
 
-// receiving some data in post global variable.
-// validate it against users table to find or fail the user account
-
 use Core\Authenticator;
-use Core\Database;
 use Core\Session;
 
-$db = new Database;
-
-$user = $db->query('select * from users where email = :email', [
-    ':email' => $_POST['email'],
-])->findOrFail();
-
-if(Authenticator::attempt($user, $_POST['password'])) {
+if((new Authenticator)->attempt($_POST['email'], $_POST['password'])) {
     redirect('/notes');
 }
 
 // Redirect using PRG pattern and pass errors
 Session::flash('errors', 'Could not find the an account matching given credentials.');
 redirect('/login');
-
-
-
