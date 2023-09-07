291b9fed7455458bc08392dab8dbaeed1930425c
aarishgilani <aarishgilani10@gmail.com>
2023-09-07 12:25:50 -0500

    Refactor extract authentication logic into a separate class.

--- core/Authenticator.php
+++ core/Authenticator.php
@@ -4,8 +4,20 @@ namespace Core;
 
 class Authenticator
 {
-    public function attempt()
+    public static function attempt($user, $password)
     {
-        
+
+        if ( password_verify($password, $user['password']) ) {
+
+            Session::put('logged_in', true);
+            Session::put('user_id', $user['id']);
+            Session::put('name', $user['name']);
+            Session::put('email', $user['email']);
+
+            return true;
+        }
+
+        return false;
+
     }
 }
